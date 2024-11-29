0861110d1ddd7fa4ace612c44f80c22b96baac45
Jay Vee <jayvee@mac.mynetworksettings.com>
2024-11-29 13:24:22 -0500

    fix: accidentally committed inputs, rolling them back and fixing gitignore

--- src/2018/index.ts
+++ src/2018/index.ts
@@ -9,6 +9,14 @@ export default (day: number, input: string[]) => {
       console.log('First dup frequency: ' + first_dup_frequency);
       break;
     }
+
+    case 2: {
+      const resulting_frequency = find_resulting_frequency(input);
+      const first_dup_frequency = find_first_dup_frequency(input);
+      console.log('Resulting frequency: ' + resulting_frequency);
+      console.log('First dup frequency: ' + first_dup_frequency);
+      break;
+    }
     
     default:
       throw new Error(`Solution for day ${day} not implemented`)
