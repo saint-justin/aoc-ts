d58905f8c52ef0db388e40a2e60ad7eeae5e11de
Justin Vaughn <justin@jvaughn.org>
2024-12-02 10:09:43 -0500

    docs: updating comments to better explain code

--- src/2024/day_02.ts
+++ src/2024/day_02.ts
@@ -1,4 +1,3 @@
-
 // 2024, Day 2 Part 1 -- https://adventofcode.com/2024/day/2
 export const count_safe_reports = (reports: string[]): number => {
   return reports.map(report => report.split(' ').map(reactor_levels => parseInt(reactor_levels, 10)))
@@ -11,7 +10,10 @@ export const count_safe_reports_with_dampening = (reports: string[]): number => 
   return reports
     .map(report => report.split(' ').map(reactor_levels => parseInt(reactor_levels, 10)))
     .filter(reactor_levels => {
-      if (check_level_safety(reactor_levels)) return true; // check full level
+      // check full level
+      if (check_level_safety(reactor_levels)) return true; 
+
+      // check levels with each stage spliced out
       for (let i=0; i<reactor_levels.length; i++) {
         const level_with_i_spliced = [...reactor_levels.slice(0, i), ...reactor_levels.slice(i+1)];
         if (check_level_safety(level_with_i_spliced)) return true;
@@ -20,12 +22,18 @@ export const count_safe_reports_with_dampening = (reports: string[]): number => 
     }).length;
 }
 
-// Helpers
+// -- Helper functions -- 
+
+// Checks whether a reactor step is increasing or decreasing
 const is_increasing = (n1: number, n2: number): boolean => n1 - n2 < 0;
+
+// Checks whether a change is considered gradual
 const is_gradual = (n1: number, n2: number): boolean => {
   const change = Math.abs(n1 - n2);
   return change >= 1 && change <= 3;
 }
+
+// checks if a given level is valid step-by-step
 const check_level_safety = (reactor_levels: number[]) => {
   if (reactor_levels[0] === reactor_levels[1]) return false; 
   const increasing_at_start = is_increasing(reactor_levels[0], reactor_levels[1]);
