db09f22bc3d7e2906329e0dca309c52d763c8e58
Justin Vaughn <justin@jvaughn.org>
2024-12-03 18:20:02 -0500

    feat: y2024 d3 parts 1+2

---
+++ src/2024/day_03.ts
@@ -0,0 +1,25 @@
+const mul_pattern = /mul\((\d+),(\d+)\)/g;
+
+export const find_mul_sum = (instruction_sets: string[]) => {
+  let sum = 0;
+  instruction_sets.forEach(line => 
+    [...line.matchAll(mul_pattern)].forEach(([_, n1, n2]) => sum += parseInt(n1) * parseInt(n2))
+  );
+  return sum;
+}
+
+export const find_enabled_mul_sum = (instruction_sets: string[]) => {
+  let sum = 0;
+  let enabled = true;
+  instruction_sets.forEach(line => 
+    [...line.matchAll(/do\(\)|don't\(\)|mul\(\d+,\d+\)/g)].forEach((full_capture) => {
+      const partial = full_capture[0].match(/(mul|don't|do)/g)![0];
+      if (partial === `do`) enabled = true;
+      if (partial === `don't`) enabled = false;
+      if (partial === `mul` && enabled) {
+        const [capture] = `${full_capture}`.matchAll(mul_pattern);
+        sum += parseInt(capture[1]) * parseInt(capture[2])
+      }
+    }));
+  return sum;
+}
