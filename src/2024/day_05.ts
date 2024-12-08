30ed58953444e9214d0a8a23e00f248d1a533ceb
Justin Vaughn <justin@jvaughn.org>
2024-12-07 21:11:09 -0500

    style: updating format for y2024 d5

--- src/2024/day_05.ts
+++ src/2024/day_05.ts
@@ -9,7 +9,6 @@ export const sum_middle_pages = (input: string[]): number => {
 // 2024, Day 5 Part 2 -- https://adventofcode.com/2024/day/5#part2
 export const sum_misordered_middle_pages = (input: string[]): number => {
   const [rules, pages_to_produce_in_update] = parse_rules_and_pages(input);
-
   const rule_as_str = rules.map(([l, r]) => `${l}|${r}`);
 
   return pages_to_produce_in_update.filter(page => !validate_page(rules, page))
@@ -18,6 +17,7 @@ export const sum_misordered_middle_pages = (input: string[]): number => {
     .reduce((acc, n) => n + acc);
 }
 
+// Parse out all the rules and pages from the initial input
 const parse_rules_and_pages = (printer_rules: string[]): [number[][], number[][]] => {
   const rules: number[][] = [];
   const pages_per_update: number[][] = [];
@@ -34,16 +34,19 @@ const parse_rules_and_pages = (printer_rules: string[]): [number[][], number[][]
   return [rules, pages_per_update]
 }
 
+// Checks whether a given rule is upheld for an arr of pages
 export const check_rule = (rules: number[], pages: number[]): boolean => {
   const [before, after]: number[] = rules.map(n => pages.indexOf(n));
   if ([before, after].includes(-1)) return true; // one of the numbers doesn't exist in pages, no need to check
   return before < after; // return check
 }
 
+// checks if every rule in an array is upheld for an arr of pages
 export const validate_page = (rules: number[][], pages: number[]): boolean => {
   return rules.every(rule => check_rule(rule, pages))
 }
 
+// sorts any array of pages according to a set of rules
 export const sort_pages_to_produce = (rules: string[], pages: number[]): number[] => {
   return pages.toSorted(
     (a: number, b: number ): number => {
